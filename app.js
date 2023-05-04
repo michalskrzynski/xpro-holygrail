6c1e97190649aa2db4f070b20c3557a9e74ecefc
michalskrzynski <michal.skrzynski@gmail.com>
2023-05-04 15:55:01 +0200

    solution files

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const fs = require('fs')
+const app = express();
+const Redis = require('ioredis')
+
+var client = new Redis();
+
+//
+// Shit from Stack overflow
+//  https://stackoverflow.com/questions/70896392/clientclosederror-the-client-is-closed-error-in-node-js-with-docker-and-redis
+// 
+// (async () => {
+//   client.connect();
+// })();
+
+// client.on('error', (err) => {
+//   console.error(err);
+// });
+
+client.mset({
+    header: 0,
+    left: 0,
+    article: 0,
+    right:0,
+    footer: 0
+  })
+  .then( () => {
+    console.log('Key set successfully.');
+  })
+  .catch( (err) => {
+    console.error( err );
+  })
+client.mget(['header', 'left', 'article', 'right', 'footer'], (err, val) => {
+  console.log( val );
+})
+
+function data() {
+  return new Promise((resolve, reject) => {
+    client.mget(['header', 'left', 'article','right','footer'], (err, value) => {
+      const data= {
+        'header': Number(value[0]),
+        'left': Number(value[1]),
+        'article': Number(value[2]),
+        'right': Number(value[3]),
+        'footer': Number(value[4])
+      };
+      err ? reject(null) : resolve(data);
+    })
+  })
+}
+
+app.use( express.static('public') );
+
+app.get( '/', function(req, res) {
+  res.send('Hello World!');
+})
+
+app.get('/data', (req, res) => {
+  data()
+    .then( data => {
+      console.log(data);
+      res.send(data);
+    }) 
+})
+
+app.get('/update/:key/:value', (req, res) => {
+  const key = req.params.key;
+  let value = Number(req.params.value);
+
+  client.get(key, function(err, reply) {
+
+      //new value
+      value = Number(reply) + value;
+      client.set(key, value);
+
+      //return data to client
+      data()
+        .then( data => {
+          console.log( data );
+          res.send(data);
+        })
+  })
+})
+
+app.listen( 3000, () => {
+  console.log('Server is running on port 3000');
+})
